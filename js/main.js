2316ed067e7273be098deb40d4aff6b4a6103a09
Alicia Gallego Gonzalez <aliciagallegonz@gmail.com>
2022-03-29 13:16:27 +0200

    javascript function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const inputs = document.querySelectorAll('.controls input');
 
 function handleChanges() {
-  console.log(this.value);
+  const suffix = this.dataset.sizing || '';
+
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
 }
 
 inputs.forEach((input) => input.addEventListener('change', handleChanges));
